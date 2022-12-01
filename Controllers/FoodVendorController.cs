5b9446b65fe2c98fb1c8791e93cbbd21956cab94
Brantly <brantly.w@gmail.com>
2022-12-01 00:41:47 -0700

    added new endpoints for food vendor based on Status and notification registration objects as well as endpoints for registering for a notification based on distance from vendor OR food type.

--- Controllers/FoodVendorController.cs
+++ Controllers/FoodVendorController.cs
@@ -1,14 +1,77 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Linq.Expressions;
 
 namespace SF_FT_DataTool.Controllers;
 
 [ApiController]
 [Route("[controller]")]
-public class FoodVendorController : ControllerBase
+public class FoodVendorController : Controller
 {
     [HttpGet]
-    public string Get()
+    [Route("ApprovedVendors")]
+    public string GetApprovedVendors()
     {
-        return FoodVendorList.ToJson();
+        return FoodVendorList.ApprovedToJson();
+    }
+    [HttpGet]
+    [Route("RequestedVendors")]
+    public string GetRequestedVendors()
+    {
+        return FoodVendorList.RequestedToJson();
+    }
+
+    [HttpPost]
+    [Route("SimulateNewVendor")]
+    public string SimulateNewVendor()
+    {
+        FoodVendorList.SimulateNewVendor(Name: Request.Form["Name"].ToString(), _FoodItems: Request.Form["FoodItems"].ToString(), _Latitude: Request.Form["Latitude"].ToString(), _Longitude: Request.Form["Longitude"].ToString());
+        return "New Vendor addition simulated.";
+    }
+
+
+    [HttpPost]
+    [Route("FoodVendorNotifications")]
+    public void NewFoodVendorNotifications()
+    {
+        //validate parsing distance as double and querytype as a one of the proper enums Food, Distance, Either
+
+        //QueryType QueryTypeValue;
+        //try
+        //{
+        //    switch (Request.Form["Type"].ToString())
+        //    {
+        //        case "Food": QueryTypeValue = QueryType.Food; break;
+        //        case "Distance": QueryTypeValue = QueryType.Distance; break;
+        //        case "Either": QueryTypeValue = QueryType.Either; break;
+        //        default: throw new Exception();
+        //    }
+        //}
+        //catch (Exception e)
+        //{
+        //    Console.WriteLine("{0} Exception caught.", e);
+        //}
+
+
+        double DistanceInMilesValue = -1;
+        try
+        {
+            DistanceInMilesValue = Convert.ToDouble(Request.Form["DistanceInMiles"].ToString());
+
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("{0} Exception caught.", e);
+        }
+
+
+        FoodVendorNotificationRegistrations.addNewRegistration(
+            _PhoneNumber: Request.Form["PhoneNumber"].ToString(),
+            _FoodItems: Request.Form["FoodItems"].ToString(),
+            _Longitude: Request.Form["Longitude"].ToString(),
+            _Latitude: Request.Form["Latitude"].ToString(),
+            _DistanceInMiles: DistanceInMilesValue,
+            _QueryType: Request.Form["Type"].ToString());
+        //FoodVendorNotifications
+        Console.WriteLine("posted something");
     }
 }
--- FoodVendor.cs
+++ FoodVendor.cs
@@ -1,20 +1,24 @@
 namespace SF_FT_DataTool;
 
 using System;
+using System.Collections;
 using System.Net;
 using System.Text.Json;
-
+using static SF_FT_DataTool.FoodVendorNotificationRegistration;
+public enum QueryType { Food, Distance, Either }
 public class FoodVendor
 {
     public string? VendorId { get; set; }
-    public string? LocationId { get; set;}
-    public string? Applicant { get; set;}
-    public string? FacilityType { get; set;}
-    public string? LocationDescription { get; set;}
-    public string? Address { get; set;}
-    public string? Permit { get; set;}
-    public string? Status { get; set;}
-    public List<string>? FoodItems { get; set;}
+    public string? LocationId { get; set; }
+    public string? Applicant { get; set; }
+    public string? FacilityType { get; set; }
+    public string? LocationDescription { get; set; }
+    public string? Latitude { get; set; }
+    public string? Longitude { get; set; }
+    public string? Address { get; set; }
+    public string? Permit { get; set; }
+    public string? Status { get; set; }
+    public List<string>? FoodItems { get; set; }
 
     //helper method to seriliaze as JSON
     public string ToJson()
@@ -23,10 +27,153 @@ public class FoodVendor
     }
 }
 
-public static class FoodVendorList{
-    public static List<FoodVendor>? FoodVendors {get; set;}
-    public static string ToJson(){
+public static class FoodVendorList
+{
+    public static List<FoodVendor> FoodVendors { get; set; } = new List<FoodVendor>();
+
+    //Serialize entire list as JSON
+    public static string AllToJson()
+    {
         return JsonSerializer.Serialize(FoodVendors);
+    }
+
+    //Filter for APPROVED only and serialize list as JSON
+    public static string ApprovedToJson()
+    {
+        var ApprovedFoodVendors = FoodVendors.FindAll(FoodVendor => FoodVendor.Status.CompareTo("APPROVED") == 0);
+        return JsonSerializer.Serialize(ApprovedFoodVendors);
+    }
+    //Filter for Requested only and serialize list as JSON
+    public static string RequestedToJson()
+    {
+        var ApprovedFoodVendors = FoodVendors.FindAll(FoodVendor => FoodVendor.Status.CompareTo("REQUESTED") == 0);
+        return JsonSerializer.Serialize(FoodVendors);
+    }
+
+    //Filter for Requested only and return as new list
+    public static List<FoodVendor> NewRequestedToList()
+    {
+        var ApprovedFoodVendors = FoodVendors.FindAll(FoodVendor => FoodVendor.Status.CompareTo("REQUESTED") == 0);
+        return ApprovedFoodVendors;
+    }
+
+    //Simulate adding a new vendor with the provided name, food items and lat/lon
+    internal static void SimulateNewVendor(string Name, string _FoodItems, string _Latitude, string _Longitude)
+    {
+        var LocationId = Guid.NewGuid().ToString();
+        var Applicant = Name;
+        var FacilityType = "Push Cart";
+        var LocationDescription = "Corner of poke and tilapia";
+        var Latitude = _Latitude;
+        var Longitude = _Longitude;
+        var Address = "52 poke street";
+        var Permit = Guid.NewGuid().ToString();
+        var Status = "REQUESTED";
+        var FoodItems = ((_FoodItems).Split(new char[] { ':', ';' })).ToList<string>();
+
+        FoodVendor vendor = new FoodVendor
+        {
+            VendorId = Guid.NewGuid().ToString(),
+            LocationId = LocationId,
+            Applicant = Applicant,
+            FacilityType = FacilityType,
+            LocationDescription = LocationDescription,
+            Latitude = Latitude,
+            Longitude = Longitude,
+            Address = Address,
+            Permit = Permit,
+            Status = Status,
+            FoodItems = FoodItems
+        };
+        FoodVendorList.FoodVendors.Add(vendor);
+
+
+        FoodVendorNotificationRegistrations.CheckRegistrationsForMatch(vendor);
+
+    }
+}
+
+public class FoodVendorNotificationRegistration
+{
+    public string? PhoneNumber { get; set; }
+    public List<string>? FoodItems { get; set; }
+    public string? Longitude { get; set; }
+    public string? Latitude { get; set; }
+    public double? DistanceInMiles { get; set; }
+    public QueryType QueryType { get; set; }
+
+}
+
+public static class FoodVendorNotificationRegistrations
+{
+    public static List<FoodVendorNotificationRegistration> Registrations { get; set; } = new List<FoodVendorNotificationRegistration>();
+
+    public static void addNewRegistration(string _PhoneNumber, string _FoodItems, string _Longitude, string _Latitude, double _DistanceInMiles, string _QueryType)
+    {
+        QueryType QueryTypeValue;
+        switch (_QueryType)
+        {
+            case "Food": QueryTypeValue = QueryType.Food; break;
+            case "Distance": QueryTypeValue = QueryType.Distance; break;
+            case "Either": QueryTypeValue = QueryType.Either; break;
+            default: throw new Exception();
+        }
+
+        FoodVendorNotificationRegistration newRegistration = new FoodVendorNotificationRegistration
+        {
+            PhoneNumber = _PhoneNumber,
+            FoodItems = _FoodItems.Split(':').ToList<string>(),
+            Longitude = _Longitude,
+            Latitude = _Latitude,
+            DistanceInMiles = _DistanceInMiles,
+            QueryType = QueryTypeValue
+        };
+        Registrations.Add(newRegistration);
+    }
+
+    public static void CheckRegistrationsForMatch(FoodVendor Vendor)
+    {
+        foreach (FoodVendorNotificationRegistration Registration in Registrations)
+        {
+            bool Matched = false;
+            //compare with each registration for a match
+            if (Registration.QueryType == QueryType.Either)
+            {
+                if (FoodTypeComparison(Vendor.FoodItems, Registration.FoodItems) || WithinRequestedDistance(Vendor, Registration)) { Matched = true; }
+            }
+            else if(Registration.QueryType == QueryType.Food)
+            {
+                if (FoodTypeComparison(Vendor.FoodItems, Registration.FoodItems)) { Matched = true; }
+            }
+            else if(Registration.QueryType == QueryType.Distance)
+            {
+                if (WithinRequestedDistance(Vendor, Registration)) { Matched = true; }                    
+            }
+
+            if (Matched)
+            {
+                SendNotification(Vendor, Registration);
+            }
+        }
+    }
+
+    public static bool WithinRequestedDistance(FoodVendor Vendor, FoodVendorNotificationRegistration Registration)
+    {
+        //check distance between Registration and vendor and compare to distanceInMiles
+        return true;
+    }
+
+    public static bool FoodTypeComparison(List<string> VendorFood, List<string> RegistrationFood)
+    {
+        //check Food items for match between Registration and vendor
+        return true;
+    }
+
+
+    public static void SendNotification(FoodVendor Vendor, FoodVendorNotificationRegistration Registration)
+    {
+        //send notice to phone number of match
+        Console.WriteLine("Notifiction sent to :" + Registration.PhoneNumber);
     }
 }
 
@@ -50,7 +197,7 @@ public static class FoodVendorUpdate
     {
         List<FoodVendor> parsedVendorsList = new List<FoodVendor>();
         string fileList = GetCSV("https://data.sfgov.org/api/views/rqzj-sfat/rows.csv");
-        
+
         //split csv up by line for processing
         string[] strings = fileList.Split(new string[] { "\n" }, StringSplitOptions.None);
 
@@ -66,7 +213,6 @@ public static class FoodVendorUpdate
             char endBlockChar = '|';
             for (int i = 0; i < line.Length; i++)
             {
-                // \"(37.794331003246846, -122.39581105302317)\"
                 if (line[i] == '(' && endBlockChar == '|')
                 {
                     tempItem.Add((char)line[i]);
@@ -93,8 +239,7 @@ public static class FoodVendorUpdate
                 }
                 else if (line[i] == ',' && !inBlock)
                 {
-                    var xyz = new string(tempItem.ToArray());
-                    items.Add(xyz);
+                    items.Add(new string(tempItem.ToArray()));
                     tempItem.Clear();
                 }
                 else
@@ -105,8 +250,7 @@ public static class FoodVendorUpdate
                 //last char in line, write data.
                 if (i == line.Length - 1)
                 {
-                    var xyz = new string(tempItem.ToArray());
-                    items.Add(xyz);
+                    items.Add(new string(tempItem.ToArray()));
                     tempItem.Clear();
                 }
             }
@@ -119,25 +263,34 @@ public static class FoodVendorUpdate
             var Applicant = items[1 + i];
             var FacilityType = items[2 + i];
             var LocationDescription = items[4 + i];
+            var Latitude = items[14 + i];
+            var Longitude = items[15 + i];
             var Address = items[5 + i];
             var Permit = items[9 + i];
             var Status = items[10 + i];
             var FoodItems = ((items[11 + i]).Split(new char[] { ':', ';' })).ToList<string>();
-            FoodVendor vendor = new FoodVendor
-            {
-                VendorId = Guid.NewGuid().ToString(),
-                LocationId = LocationId,
-                Applicant = Applicant,
-                FacilityType = FacilityType,
-                LocationDescription = LocationDescription,
-                Address = Address,
-                Permit = Permit,
-                Status = Status,
-                FoodItems = FoodItems
-            };
-            parsedVendorsList.Add(vendor);
-        }
 
-        FoodVendorList.FoodVendors = parsedVendorsList;
+            //Exclude vendors that are expired or suspended
+            if (Status.CompareTo("EXPIRED") != 0 && Status.CompareTo("SUSPEND") != 0)
+            {
+                FoodVendor vendor = new FoodVendor
+                {
+                    VendorId = Guid.NewGuid().ToString(),
+                    LocationId = LocationId,
+                    Applicant = Applicant,
+                    FacilityType = FacilityType,
+                    LocationDescription = LocationDescription,
+                    Latitude = Latitude,
+                    Longitude = Longitude,
+                    Address = Address,
+                    Permit = Permit,
+                    Status = Status,
+                    FoodItems = FoodItems
+                };
+
+                FoodVendorList.FoodVendors.Add(vendor);
+            }
+        }
     }
 }
+
